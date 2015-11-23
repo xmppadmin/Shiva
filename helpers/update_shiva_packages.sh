a43c45f6a2761c18dd1918e13b49b3d35417ee6a
Michal Merta <merta.m@gmail.com>
2015-11-23 16:30:17 +0100

    fixed update script

--- helpers/update_shiva_packages.sh
+++ helpers/update_shiva_packages.sh
@@ -1,4 +1,6 @@
  #!/bin/sh
 
  cp WORK_PATH/analyzer/core/*.py WORK_PATH/shiva/shivaAnalyzer/lib/python2.7/site-packages/lamson
+ 
+ cp WORK_PATH/analyzer/core/shiva_phishing/*.py WORK_PATH/shiva/shivaAnalyzer/lib/python2.7/site-packages/lamson/shiva_phishing/
 
