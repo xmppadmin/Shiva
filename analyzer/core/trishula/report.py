77d42a695b1b1278df472961108db1aacc46abf1
Michal Merta <merta.m@gmail.com>
2015-12-17 11:55:28 +0100

    refactored report sending

--- analyzer/core/trishula/report.py
+++ analyzer/core/trishula/report.py
@@ -3,7 +3,15 @@ Module send email notification about phishing emails
 """
 
 import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.message import MIMEMessage
+from email.utils import formataddr
+from email import message_from_file
+
 import lamson.server
+import backend_operations
+
 
 from mako.template import Template
 
@@ -15,38 +23,96 @@ def send_phishing_report(mailFields):
     
     smtphost = lamson.server.shivaconf.get('analyzer', 'relayhost')
     smtpport = lamson.server.shivaconf.get('analyzer', 'relayport')
-
-
-    template_str = """From: SHIVA honeypot <${honeypot_email}>
-To: ${recipient_email}
-MIME-Version: 1.0
-Content-Type: text/plain; charset=utf-8
-Subject: Test automaticke detekce phishingu
-
-SHIVA honeypot: Byl zachycen podezrely email
-
-Detaily:
-  Predmet: ${phishing_subject}
-  Odesilatel: ${phishing_from}
-  Prijemce: ${phishing_to}
-  Odkaz: http://${web_iterface_url}/view_email?email_id=${email_id}
-"""
     
+    msg = MIMEMultipart('mixed')
+    msg['Subject'] = "Automatic phishing detection report"
+    msg['From'] = formataddr(('SHIVA honeypot', report_from,))
+    msg['To'] = formataddr(('', report_to,))
+
+
+
+    if not mailFields['s_id']:
+        return
+
+    raw_path = lamson.server.shivaconf.get('analyzer', 'rawspampath')
+    phish_path = raw_path + 'phishing/'
+    
+    
+    phish_file_name = ''
+    from os import walk
+    for _, _, filenames in walk(phish_path):
+        for filename in filenames:
+            if filename.startswith(mailFields['s_id']):
+                phish_file_name = filename
+                break
+            
+    if not phish_file_name:
+        return
+    
+    
+    links = backend_operations.get_permament_url_info_for_email(mailFields['s_id'])
+    
+    has_phishtank = any(map(lambda a: True if a['InPhishTank'] else False ,links))
+    has_googlesba = any(map(lambda a: a['GoogleSafeBrowsingAPI'],links))
+
+    template_str = """
+SHIVA honeypot: suspicious email was caught
+
+Overview:
+  Subject: ${phishing_subject}
+  Sender: ${phishing_from}
+  Recipient: ${phishing_to}
+  Link: http://${web_iterface_url}/view_email?email_id=${email_id}
+  
+  % if in_phishtank == True:
+    Links in PhishTank:
+    % for link_info in links_info:
+    % if link_info['InPhishTank'] == True:
+      ${link_info['raw_link']}
+    %endif
+    % endfor
+  % endif      
+
+  % if in_googlesba == True:
+    Dangerous links in Gogole Safe Browsing API:
+    % for link_info in links_info:
+    % if link_info['GoogleSafeBrowsingAPI'] == True:
+      ${link_info['raw_link']}
+    %endif
+    % endfor
+  % endif  
+
+  
+"""    
     template = Template(template_str, output_encoding='utf-8', encoding_errors='replace')
 
-    message = template.render(honeypot_email=report_from,
-                                recipient_email=report_to,
-                                phishing_subject=mailFields['subject'],
-                                phishing_to=mailFields['to'],
+    text_message = template.render(phishing_to=mailFields['to'],
                                 phishing_from=mailFields['from'],
+                                phishing_subject=mailFields['subject'],
                                 web_iterface_url=domain_root,
-                                email_id=mailFields['s_id'])
+                                email_id=mailFields['s_id'],
+                                in_phishtank=has_phishtank,
+                                in_googlesba=has_googlesba,
+                                links_info=links)
      
+
+    textpart = MIMEText(text_message, 'plain', 'utf-8')
+    textpart['Content-Transfer-Encoding: 8bit']
+    
+    phish_file = open(phish_path + phish_file_name, 'rb')
+    
+    rfc822part = MIMEMessage(message_from_file(phish_file))
+    phish_file.close()
+    
+    rfc822part['Content-Description'] = 'Original Message'
+    rfc822part['Content-Disposition'] = 'inline'
+    msg.attach(textpart)
+    msg.attach(rfc822part)
 
        
     try:
         smtpobj = smtplib.SMTP(smtphost, smtpport)
-        smtpobj.sendmail(report_from, report_to, message)
+        smtpobj.sendmail(report_from, report_to, msg.as_string())
         print "\n\t[+] Phishing notification sent successfully"
     except smtplib.SMTPException:
         print "\n\t[!] Error: unable to send error phishing notification mail via Exim4"   
